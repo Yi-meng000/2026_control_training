87693f1257814f6c876a50d7323c1e6e50d303e8
Frt001 <2067314783@qq.com>
2026-08-25 17:25:47 +0800

    增加蓝牙调用

--- IRQ/Src/UART_IRQHandler.c
+++ IRQ/Src/UART_IRQHandler.c
@@ -8,36 +8,39 @@
  */
 #include "UART_IRQHandler.h"
 #include "EXTI_IRQHandler.h"
+#include "BlueTooth.h"
 
-uint8_t rx_buffer[100] = {0};
-uint8_t tx_buffer[100] = {0};
+uint8_t u1_rx_buffer[1] = {0};
+uint8_t u2_rx_buffer[5] = {0};
+uint8_t tx_buffer[5] = {0};
 
 void UART_Start_Recieve(void)
 {
-    // HAL_UART_Receive_IT(&huart1, rx_buffer, 5);
-    // HAL_UARTEx_ReceiveToIdle_IT(&huart1, rx_buffer, sizeof(rx_buffer));
-    // HAL_UART_Receive_DMA(&huart1, rx_buffer, 5);
-    HAL_UARTEx_ReceiveToIdle_DMA(&huart1, rx_buffer, sizeof(rx_buffer));
-    // HAL_UART_Receive_IT(&huart2, rx_buffer, 5);
-    // HAL_UARTEx_ReceiveToIdle_IT(&huart2, rx_buffer, sizeof(rx_buffer));
-    // HAL_UART_Receive_DMA(&huart2, rx_buffer, 5);
-    HAL_UARTEx_ReceiveToIdle_DMA(&huart2, rx_buffer, sizeof(rx_buffer));
+    // HAL_UART_Receive_IT(&huart1, u1_rx_buffer, 1);
+    // HAL_UARTEx_ReceiveToIdle_IT(&huart1, u1_rx_buffer, sizeof(rx_buffer));
+    HAL_UART_Receive_DMA(&huart1, u1_rx_buffer, 1);
+    // HAL_UARTEx_ReceiveToIdle_DMA(&huart1, u1_rx_buffer, sizeof(u1_rx_buffer));
+    // HAL_UART_Receive_IT(&huart2, u2_rx_buffer, 5);
+    // HAL_UARTEx_ReceiveToIdle_IT(&huart2, u2_rx_buffer, sizeof(rx_buffer));
+    // HAL_UART_Receive_DMA(&huart2, u2_rx_buffer, 5);
+    HAL_UARTEx_ReceiveToIdle_DMA(&huart2, u2_rx_buffer, sizeof(u2_rx_buffer));
 }
 
 void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart)
 {
     if (huart->Instance == USART1)
     {
-
-        // HAL_UART_Receive_IT(&huart1, rx_buffer, 5);
-        HAL_UART_Receive_DMA(&huart1, rx_buffer, 5);
+        Deal_RxPack(u1_rx_buffer[0]);
+        
+        // HAL_UART_Receive_IT(&huart1, u1_rx_buffer, 1);
+        HAL_UART_Receive_DMA(&huart1, u1_rx_buffer, 1);
 
     }
     if (huart->Instance == USART2)
     {
 
-        // HAL_UART_Receive_IT(&huart2, rx_buffer, 5);
-        HAL_UART_Receive_DMA(&huart2, rx_buffer, 5);
+        // HAL_UART_Receive_IT(&huart2, u2_rx_buffer, 5);
+        HAL_UART_Receive_DMA(&huart2, u2_rx_buffer, 5);
 
     }
 
@@ -48,15 +51,15 @@ void HAL_UARTEx_RxEventCallback(UART_HandleTypeDef *huart, uint16_t Size)
     if (huart->Instance == USART1) 
     {
 
-        // HAL_UARTEx_ReceiveToIdle_IT(&huart1, rx_buffer, sizeof(rx_buffer));
-        HAL_UARTEx_ReceiveToIdle_DMA(&huart1, rx_buffer, sizeof(rx_buffer));
+        // HAL_UARTEx_ReceiveToIdle_IT(&huart1, u1_rx_buffer, sizeof(rx_buffer));
+        HAL_UARTEx_ReceiveToIdle_DMA(&huart1, u1_rx_buffer, sizeof(u1_rx_buffer));
 
     }
     if (huart->Instance == USART2) 
     {
         
-        // HAL_UARTEx_ReceiveToIdle_IT(&huart2, rx_buffer, sizeof(rx_buffer));
-        HAL_UARTEx_ReceiveToIdle_DMA(&huart2, rx_buffer, sizeof(rx_buffer));
+        // HAL_UARTEx_ReceiveToIdle_IT(&huart2, u2_rx_buffer, sizeof(rx_buffer));
+        HAL_UARTEx_ReceiveToIdle_DMA(&huart2, u2_rx_buffer, sizeof(u2_rx_buffer));
 
     }
 }
